9603e4e3368f15cc4f33c2067d484d5cbd0c470a
Philip Beadle <philip.beadle@live.com.au>
2019-12-16 20:29:59 +1100

    Install vuetify-cli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '../components/HelloWorld';
 
 export default {
-  name: 'home',
+  name: 'Home',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
